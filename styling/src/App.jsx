079701e0c99510d831c2858c3b2086b705469fb9
SsongCh94 <thddkwl94@gmail.com>
2023-02-26 01:47:52 +0900

    map 과 filter 를 이용한 컴포넌트 생성

---
+++ styling/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const testArr = ["감자", "고구마", "오이", "가지", "옥수수"];
+
+  return (
+    <div className="totalStyle">
+      {
+        testArr.filter((item) => {
+          
+          return item != '오이'
+        })
+        .map((item) =>{
+          return <div className="component-style">{item}</div>;
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
