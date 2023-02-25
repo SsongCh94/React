a3efafc6496cf0355dd1fa7aa53f6baa4c59e0ba
SsongCh94 <thddkwl94@gmail.com>
2023-02-25 09:24:19 +0900

    삼항연산자, style 태그 추가

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,31 +1,26 @@
 import React from "react";
 
-function GrandFather() {
-  return (
-    <div>
-      <div>자식 부른 아빠 부른 할아버지다.</div>
-      <Father />
-    </div>
-  );
-}
-
-function Father() {
-  return (
-    <div>
-      <div>자식 부른 아빠다.</div>
-      <Son />
-    </div>
-  );
-}
-
-function Son() {
-  return <button>자식이다.</button>;
-}
-
 function App() {
+  const number = 1;
+
+  const pTagStyle = {
+    color: 'red',
+  };
+
+  const arr = [1, 2, 3, 4, 5];
+  const arr1 = arr.map(x => x+1);
+
+
   return (
     <div>
-      <GrandFather />
+      <p style = {pTagStyle}>안녕하세요 리액트입니다.</p>
+      {/* 주석을 사용하는 방법 */}
+      {/* 삼항 연산자 사용 */}
+      <p style ={pTagStyle}>
+        {number > 10 ? number + "은 10보다 크다." : number + "은 10보다 작다."}
+      </p>
+      <p>{arr}</p>
+      <p>{arr1}</p>
     </div>
   );
 }
