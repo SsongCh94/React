c3ffeec1c321feaec5f5191fdb139edf3a1e8f97
SsongCh94 <thddkwl94@gmail.com>
2023-02-25 12:16:27 +0900

    props children 이용, 응용 방법!

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,32 +1,34 @@
 import React from "react";
+import Layout from "Layout";
 
 
-//props 라는 이름으로 input 을 받아서 위에서 내려온 데이터를 전달받는다.
-//props 를 객체 형태로 전달받게 된다.
-function Son(props) {
-  console.log('props', props)
-  return <button>{props.gfName}의 손자이다.</button>;
-}
+//Children 용법
 
-//부모컴포넌트에서 자식컴포넌트로 데이터를 전달했다.
-function Father(p) {
-  const name = '송아빠';
-  const gfName = p.grandFatherName;
-  console.log('p', p)
-  return <Son gfName={gfName}/>;
-}
 
-function GrandFather() {
-  const name = '송할배';
-  return <Father grandFatherName={name}/>;
-}
+//children 기본 틀
+
+// function App() {
+//   return <User>안녕하세요!</User>;
+// }
+
+// function User(props) {
+//   console.log('props', props)   //  {children: '안녕하세요!'}
+//   return <div></div>;
+// }
+
+//////////////////////////////////
+
+//응용방법 - 최상단에 import Layout from "Layout" 을 넣어 Layout.js 를 임포트
+
 
 function App() {
   return (
-    <div>
-      <GrandFather />
-    </div>
+    <Layout>
+    <div>App 컴포넌트에서 보낸 값 입니다.</div>
+  </Layout>
   );
 }
+
+
 
 export default App;
