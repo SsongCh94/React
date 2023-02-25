0de7fe81d141505cc70c760ee3674ea8264964c3
SsongCh94 <thddkwl94@gmail.com>
2023-02-25 18:28:40 +0900

    useState 아이디 비밀번호 입력받기

--- week-1/src/App.jsx
+++ week-1/src/App.jsx
@@ -1,20 +1,69 @@
 import React, { useState } from "react";
 
+// function App() {
+//   const [id, setId] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   return (
+//     <div>
+//       아이디 : <input
+//       type={'text'}
+//       value={id}
+//       onChange={function(event){
+//         setId(event.target.value)
+//       }}
+//       />
+//       <br />
+//       비밀번호 : <input
+//       type={'password'}
+//       value={password}
+//       onChange={function(event){
+//         setPassword(event.target.value)
+//       }}
+//       />
+//       <br />
+//       <button onClick={function (){
+//         alert('입력된 아이디 {id} 입력된 비번 {password}')
+//       }}>로그인</button>
+//     </div>
+//   );
+// }
+
 function App() {
-  const [fruit, setFruit] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onIdChangeHandler = (event) => {
+    setId(event.target.value);
+  };
+
+  const onPwChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div>
-      과일 :{" "}
-      <input
-        value={fruit}
-        onChange={function (event) {
-          // console.log('event', event.target.value);
-          setFruit(event.target.value);
+      <div>
+        아이디 : <input type={"text"} value={id} onChange={onIdChangeHandler} />
+      </div>
+      <div>
+        비밀번호 :{" "}
+        <input
+          type={"password"}
+          value={password}
+          onChange={onPwChangeHandler}
+        />
+      </div>
+
+      <button
+        onClick={() => {
+          alert(`입력된 아이디 ${id}, 입력된 비번 ${password}`);
+          setId('');
+          setPassword('');
         }}
-      />
-      <br /> <br />
-      {fruit}
+      >
+        로그인
+      </button>
     </div>
   );
 }
