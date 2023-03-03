44561e821fc9d8c8573285fc7b8171cc11ffc2d2
SsongCh94 <thddkwl94@gmail.com>
2023-03-03 15:20:28 +0900

    CSS RESET 방법.

--- week-2/styled-components/src/App.jsx
+++ week-2/styled-components/src/App.jsx
@@ -1,49 +1,17 @@
-import "./App.css";
-import styled from "styled-components";
-import GlobalStyle from "./GlobalStyle";
+import React from 'react'
+import './reset.css';
 
-const StContainer = styled.div`
-  /* display: flex; */
-`
-
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor};
-  margin: 20px;
-`;
-
-// 박스의 색
-const boxList = ['red', 'blue', 'green', 'black']
-
-// 색을 넣으면, 이름을 반환
-const getBoxName = (color) => {
-  switch (color) {
-    case 'red':
-      return '빨간 박스';
-    case 'green':
-      return '초록 박스';
-    case 'blue':
-      return '파란 박스';
-    default:
-      return '검정 박스';
-  }
-}
- 
 function App() {
   return (
-    <>
-    <GlobalStyle />
-    <StContainer>
-    {
-      boxList.map((box) => {
-        return <StBox borderColor={box} >{getBoxName(box)}</StBox>
-      })
-    }
-    </StContainer>
-    </>
-    
-  );
+    <div>
+      <span>Default Style 테스트</span>
+      <h1>이건 h1 태그입니다.</h1>
+      <p>나는 p 태그입니다.</p>
+    </div>
+  )
 }
 
-export default App;
+// default style 
+// CSS RESET
+
+export default App
