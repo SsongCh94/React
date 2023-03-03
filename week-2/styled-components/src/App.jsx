f13b442572f6a3cc1248ee72e9592c52223c8790
SsongCh94 <thddkwl94@gmail.com>
2023-03-03 14:50:27 +0900

    styled-components 기본 문법과 props, map을 이용한 응용

---
+++ week-2/styled-components/src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import styled from "styled-components";
+
+const StContainer = styled.div`
+  display: flex;
+`
+
+const StBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+  margin: 20px;
+`;
+
+// 박스의 색
+const boxList = ['red', 'blue', 'green', 'black']
+
+// 색을 넣으면, 이름을 반환
+const getBoxName = (color) => {
+  switch (color) {
+    case 'red':
+      return '빨간 박스';
+    case 'green':
+      return '초록 박스';
+    case 'blue':
+      return '파란 박스';
+    default:
+      return '검정 박스';
+  }
+}
+
+function App() {
+  return (
+    <StContainer>
+    {
+      boxList.map((box) => {
+        return <StBox borderColor={box} >{getBoxName(box)}</StBox>
+      })
+    }
+    </StContainer>
+  );
+}
+
+export default App;
