963dbf00acd883c51d8c216a5badcd5183badb9e
SsongCh94 <thddkwl94@gmail.com>
2023-03-07 01:48:21 +0900

    동적 라우팅... 너무어렵다 계속보자

--- week-2/router-app/src/pages/Works.jsx
+++ week-2/router-app/src/pages/Works.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { data } from "../shared/data";
 
 function Works() {
-  // const navigate = useNavigate();
-  const location = useLocation();
-
-  // console.log("location", location);
-
   return (
     <div>
-      Works
-      <br />
-      <button
-      // onClick={() => navigate("/")}
-      >
-        {" "}
-        Home으로 이동
-      </button>
-      <br />
-      <Link to={"/contact"}>contact 페이지로 이동하기</Link>
+      <h3>할일 목록</h3>
+      {data.map((item) => {
+        return (
+          <div key={item.id}>
+            {item.id} &nbsp;
+            <Link to={`/works/${item.id}`}>{item.todo}</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- week-2/router-app/src/shared/router.js
+++ week-2/router-app/src/shared/router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Home from '../pages/Home';
+import Work from "../pages/Work";
 import Works from "../pages/Works";
 
 
@@ -13,6 +14,7 @@ const Router = () => {
             <Route path="about" element={<About />}/>
             <Route path="contact" element={<Contact />} />
             <Route path="works" element={<Works />}/>
+            <Route path="works/:id" element={<Work />}/>
         </Routes>
         </BrowserRouter>
     )
