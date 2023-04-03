d7a63a349522d01b6c79efdd2b602d5b7db2a931
SsongCh94 <thddkwl94@gmail.com>
2023-04-04 02:07:55 +0900

    Ts 객체, 배열, 고정값

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -17,4 +17,10 @@ interface Add {
 
 const add: Add = (x, y) => x + y;
 
-// const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 };
+const f: true = true;
+const z: 5 = 5;
+
+const arr: string[] = ['123', '456'];
+const arr2: Array<number> = [123, 456]; //Generics
+const arr3: [number, number, string] = [123, 456, 'hello']; //튜플 tuple
+const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
