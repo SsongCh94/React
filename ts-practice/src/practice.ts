648f1e694ea014bb79932752fc06d94b22115365
SsongCh94 <thddkwl94@gmail.com>
2023-04-04 10:29:39 +0900

    Ts의 타입 추론

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,26 +1,28 @@
-const a: string = '5';
-const b: number = 5;
-const c: boolean = true;
-const d: undefined = undefined;
-const e: null = null;
+const a = '5';
+const b = 5;
+const c = true;
+const d = undefined;
+const e = null;
 
-// function add(x: number, y: number): number {
-// 	return x + y;
-// }
+let aa = 123;
+aa = 'hello' as unknown as number;
 
-// type Add = (x: number, y: number) => number;
-// const add: Add = (x, y) => x + y;
-
+try {
+	const array: string[] = [];
+	array.push('hello');
+} catch (error) {
+	error;
+}
 interface Add {
 	(x: number, y: number): number;
 }
 
-const add: Add = (x, y) => x + y;
+// const add: Add = (x, y) => x + y;
 
 const f: true = true;
 const z: 5 = 5;
 
-const arr: string[] = ['123', '456'];
+const arr = ['123', '456'];
 const arr2: Array<number> = [123, 456]; //Generics
 const arr3: [number, number, string] = [123, 456, 'hello']; //튜플 tuple
 const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
